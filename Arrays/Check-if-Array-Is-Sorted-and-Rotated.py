fa5ceb913c8246195355eebca32e1cecd6e93c12
Teja Sajja <tsajja75@gmail.com>
2025-04-06 17:53:50 -0500

    [1752] [Time Beats: 100.00%] [Memory Beats: 10.89%] - LeetPush

--- Arrays/Check-if-Array-Is-Sorted-and-Rotated.py
+++ Arrays/Check-if-Array-Is-Sorted-and-Rotated.py
@@ -3,15 +3,16 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
+        Lead: this soution is designed based on possible inversion (sorted array should only    
+        have only one inversion) 
         """
+
+
         cnt=0
         for i in range(1, len(nums)):
             if nums[i]<nums[i-1]:
                 cnt+=1
-
-        print(cnt)
         if nums[len(nums)-1]>nums[0]:
             cnt+=1
-        print(cnt)
         return cnt<=1
         
