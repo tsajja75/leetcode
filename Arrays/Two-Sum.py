31d195b8bb29eb7d4c0ca9fab3efd7ecc858b689
Teja Sajja <tsajja75@gmail.com>
2025-04-18 19:56:51 -0500

    [1] [Time Beats: 100.00%] [Memory Beats: 61.65%] - LeetPush

--- Arrays/Two-Sum.py
+++ Arrays/Two-Sum.py
@@ -5,11 +5,35 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         \\\
+        ## map approch failed in case of duplicates
+        # m = {}
+        # for i in range(len(nums)):
+        #     if i not in m:
+        #          m[target-nums[i]]= i
 
-        
+
+        # for k,v in m.items():
+        #     print(k,v)
+        #     if k in nums:
+        #         return [v,nums.index(k)]
+
+
+        ## two pass hashmap
+
+        # m ={}
+        # for i in range(len(nums)):
+        #     m[nums[i]] = i
+        # for i in range(len(nums)):
+        #     c = target-nums[i]
+        #     if c in m and m[c]!=i:
+        #         return [i, m[c]]
+        # return[]
+
+        ## onepass hashmap
         m ={}
-        for  i,x in enumerate(nums):
-            y = target -x 
-            if y in m:
-                return [m[y],i]
-            m[x] = i
+        for i in range(len(nums)):
+            c = target-nums[i]
+            if c in m:
+                return [i,m[c]]
+            m[nums[i]]=i
+        return []
